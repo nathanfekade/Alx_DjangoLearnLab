c70dfe9f851293b06de0231962bb99a888fb0852
nathanfekade <nathanfekade276@gmail.com>
2025-02-28 09:43:27 +0300

    added on views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Author, Book, Librarian
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 def list_books(request):
     books = Book.objects.all()
