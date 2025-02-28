3d156e9859c699ad6aeb23f0b5bb7085855b5d93
nathanfekade <nathanfekade276@gmail.com>
2025-02-28 09:44:55 +0300

    added template name

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('booklist/',list_books, name='booklist'),
     path('bookdetail/',LibraryDetailView.as_view(), name='book-detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
