4ce6682059a9ea9dd68daa26ad767068999b6eb0
nathanfekade <nathanfekade276@gmail.com>
2025-02-28 10:09:45 +0300

    Set Up Role-Based Views and Configure URL Patterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
+from .views import admin_view, librarian_view, member_view
 from . import views
 urlpatterns = [
     path('booklist/',list_books, name='booklist'),
     path('bookdetail/',LibraryDetailView.as_view(), name='book-detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Author, Book, Librarian
+from django.shortcuts import render, redirect
+from .models import Author, Book, Librarian, UserProfile
 from django.views.generic.detail import DetailView
 from .models import Library
 from django.contrib.auth import login
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books(request):
     books = Book.objects.all()
@@ -37,3 +38,29 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 UserCreationForm()
+
+def check_role(role):
+    def role_check(user):
+        if not user.is_authenticated:
+            return False
+        try:
+            user_profile = user.userprofile
+            return user_profile.role == role
+        except UserProfile.DoesNotExist:
+            return False
+    return user_passes_test(role_check)
+
+# Admin view
+@check_role('Admin')
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html', {'message': 'Welcome, Admin!'})
+
+# Librarian view
+@check_role('Librarian')
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html', {'message': 'Welcome, Librarian!'})
+
+# Member view
+@check_role('Member')
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html', {'message': 'Welcome, Member!'})
