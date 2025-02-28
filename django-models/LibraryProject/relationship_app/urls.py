94f653752f90156c347534dad0aaf6d9a0a197d0
nathanfekade <nathanfekade276@gmail.com>
2025-02-28 10:31:55 +0300

    added url path

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
-from .views import admin_view, librarian_view, member_view
+from .views import admin_view, librarian_view, member_view, delete_book
 from . import views
 urlpatterns = [
     path('booklist/',list_books, name='booklist'),
@@ -13,4 +13,7 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+    path('add_book/', admin_view, name='add_book'),  
+    path('edit_book/', librarian_view, name='edit_book'),  
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),  
 ]
