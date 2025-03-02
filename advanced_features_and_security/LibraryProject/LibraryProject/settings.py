ef1254ee5e1399dac1ccf001d0f7f5624d03b329
nathanfekade <nathanfekade276@gmail.com>
2025-03-02 11:25:36 +0300

    Update Settings to Use the Custom User Model

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "bookshelf.CustomUser"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
