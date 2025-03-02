992ebc68bd40d7863056e49df3d497e85f0f37c0
nathanfekade <nathanfekade276@gmail.com>
2025-03-02 11:54:44 +0300

    added on admin site

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Book, CustomUser
 
 class BookAdmin(admin.ModelAdmin):
   list_display = ("title", "author", "publication_year")
-  search_fields = ('author', 'text')
+  search_fields = ('author', 'author')
   list_filter = ('publication_year',)
 
+class CustomUserAdmin(admin.ModelAdmin):
+  list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+  list_filter = ('is_staff', 'is_superuser', 'date_of_birth')
+  search_fields = ('username', 'email')
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
