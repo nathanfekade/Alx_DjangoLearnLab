436340f378768b06b7f005717abfa67c5ad41c07
nathanfekade <nathanfekade276@gmail.com>
2025-03-09 14:24:58 +0300

    updated user

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
@@ -41,7 +42,7 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     )
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
     def __str__(self):
