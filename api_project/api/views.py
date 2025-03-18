92f86eb1680c98bb43dd1ffe6681c0f69e997076
nathanfekade <nathanfekade276@gmail.com>
2025-03-18 13:44:18 +0300

    added authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class BookViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
