5d56d4eb42051b23d977966af67b482142cac9ff
nathanfekade <nathanfekade276@gmail.com>
2025-03-18 13:17:20 +0300

    chaged to using ModelViewset

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import BookList
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
-]
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='books' )
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     # path('books/', BookList.as_view(), name='book-list'),  
+# ]
