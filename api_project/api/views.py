38ba0c384e081db6a629dc9485766bba6b280f71
nathanfekade <nathanfekade276@gmail.com>
2025-03-18 13:25:34 +0300

    re arranged places

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,10 +3,11 @@ from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListAPIView):
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
