00e318093fdcad2a7f07305211e576b17bd17117
nathanfekade <nathanfekade276@gmail.com>
2025-03-18 13:20:56 +0300

    included both url for viewset and generics

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'books', BookViewSet, basename='books' )
+router.register(r'books_all', BookViewSet, basename='book_all')
 
-urlpatterns = router.urls
 
-# urlpatterns = [
-#     # path('books/', BookList.as_view(), name='book-list'),  
-# ]
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),  
+    path('', include(router.urls))
+]
