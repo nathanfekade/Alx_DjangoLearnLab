2e6beaf74c49363fa9200847b9c2f99755d923fe
nathanfekade <nathanfekade276@gmail.com>
2025-03-20 12:52:05 +0300

    added book and author serializer

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Book, Author
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+class AuthorSerializer(serializers.ModelSerializer):
+
+    books = BookSerializer(many=True, read_only=True)
+    class Meta:
+        model = Author
+        fields ="__all__"
