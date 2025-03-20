3c5e1c6db05260ec8d58b561674effe139e9e840
nathanfekade <nathanfekade276@gmail.com>
2025-03-20 14:38:33 +0300

    added search

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,12 +3,15 @@ from rest_framework import generics
 from .models import Book    
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
 
 class BookListView(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author', 'publication_year']
 
 class BookDetailView(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
