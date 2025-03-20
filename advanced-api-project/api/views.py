fc62b1571c4dcf13a2d8da0775bf55120d4c18ca
nathanfekade <nathanfekade276@gmail.com>
2025-03-20 14:15:51 +0300

    added is authenticated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,26 +2,37 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book    
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class BookListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookDetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'pk'
 
 class BookCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookUpdateView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'pk'
 
 class BookDeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'pk'    
