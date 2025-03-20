5e565616062dbbfe963aa949c3684c8ba9f6113a
nathanfekade <nathanfekade276@gmail.com>
2025-03-20 13:02:12 +0300

    added year validation

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from .models import Book, Author
+import datetime
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+    
+    def validate(self, data):
+        today = datetime.date.today()
+        year = today.strftime('%y')
+        if data['publication_year'] > year:
+            raise serializers.ValidationError("ERROR: the year can not be in the future")
+        return data
 
 class AuthorSerializer(serializers.ModelSerializer):
 
