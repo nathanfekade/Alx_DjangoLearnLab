931b0ba403b63f6bf26ae7a7cd44a7c537387302
nathanfekade <nathanfekade276@gmail.com>
2025-03-21 12:29:14 +0300

    changed to using apitestcase

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,7 +1,20 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 from .models import Book
 
-class BookTestCase(TestCase):
+class BookTestCase(APITestCase):
     def setUp(self):
-        Book.objects.create('Animal farm', 1978)
-    
+        self.test_item = Book.objects.create('Animal farm', 1978)
+
+        self.list_url = reverse('book-list')
+        self.create_url = reverse('book-create')
+        self.retrieve_url = reverse('book-detail', args=[self.test_item.id])
+        self.update_url = reverse('book-update', args=[self.test_item.id])
+        self.delete_url = reverse('book-delete', args=[self.test_item.id])
+
+    def test_list_items(self):
+        
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
