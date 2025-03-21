f256c7a1bf789c3e59822bea32dbbd4bfbdf417f
nathanfekade <nathanfekade276@gmail.com>
2025-03-21 12:48:09 +0300

    added test database and tested user login

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -2,11 +2,19 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
 from .models import Book
+from django.contrib.auth.models import User
 
 class BookTestCase(APITestCase):
     def setUp(self):
         self.test_item = Book.objects.create('Animal farm', 1978)
 
+        self.user = User.objects.create_user(
+            username = 'testuser',
+            password = '123'
+        )
+
+        self.client.login(username = 'testuser', password = '123')
+        
         self.list_url = reverse('book-list')
         self.create_url = reverse('book-create')
         self.retrieve_url = reverse('book-detail', args=[self.test_item.id])
