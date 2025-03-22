25a21825af89feede044e8b7197ab305d2d9fce5
nathanfekade <nathanfekade276@gmail.com>
2025-03-22 13:54:52 +0300

    created static files and configured them

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def base(request):
+    return render(request, 'blog/base.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.post)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('base')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
