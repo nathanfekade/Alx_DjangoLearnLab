31b53e830f8643335e4c2ba060577608b6afa1d5
nathanfekade <nathanfekade276@gmail.com>
2025-03-22 14:11:53 +0300

    fixed url profile

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.base, name='base'),
     path('register/', views.register, name='register'),
-    path('profile', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
 ]
