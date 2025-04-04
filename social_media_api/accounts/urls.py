9119b20931c897f3f15fa46b490ec7a43e14362f
nathanfekade <nathanfekade276@gmail.com>
2025-04-04 15:23:33 +0300

    added profile

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import RegisterView, LoginView, RetrieveTokenView
+from .views import RegisterView, LoginView, ProfileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('token/', RetrieveTokenView.as_view(), name='retrieve_token'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,10 +4,10 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
-from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 class RegisterView(APIView):
-    
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
@@ -32,18 +32,11 @@ class LoginView(APIView):
                 return Response({'token': token.key}, status=status.HTTP_200_OK)
             return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
         
-class RetrieveTokenView(APIView):
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            user = authenticate(
-                username = serializer.validated_data['username'],
-                password = serializer.validated_data['password']
-            )
-            if user: 
-                if Token.objects.filter(user=user).exists():
-                    token = Token.objects.get(user=user)
-                else:
-                    token = Token.objects.create(user=user)
-                return Response({'token': token.key}, status=status.HTTP_200_OK)
-            return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+class ProfileView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serialzier = UserSerializer(user)
+        return Response(serialzier.data, status=status.HTTP_200_OK)
