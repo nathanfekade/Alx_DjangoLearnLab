6c5616963b5da341fe8d11508983a00e9bf8f671
nathanfekade <nathanfekade276@gmail.com>
2025-04-04 15:56:29 +0300

    created app posts and models called post and comment

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
