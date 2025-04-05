1013c8304cb55be26f05eb6859e262e0046de5d5
nathanfekade <nathanfekade276@gmail.com>
2025-04-05 11:54:25 +0300

    added static

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -128,6 +128,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/ 'staticfiles'
+
+
 REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
