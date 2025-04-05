46d7f3c64f009f9fce0619e6be1a5e133d511964
nathanfekade <nathanfekade276@gmail.com>
2025-04-05 09:53:27 +0300

    created notifications app and added liked model

---
+++ social_media_api/notifications/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+class Notifications(models.Model):
+    recipient = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='notifications')
+    actor = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='actions')
+    verb = models.CharField(max_length=50)
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True)
+    target_object_id = models.PositiveBigIntegerField(null=True, blank=True)
+    target = GenericForeignKey('target_content_type', 'target_object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -14,4 +14,10 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
+class Like(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+
+    class Meta:
+        unique_together = ('user', 'post')
